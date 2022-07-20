2a7c9322fd33006244ad7c8edba4a0cb3484b970
Riley <61441946+isoomni@users.noreply.github.com>
2022-07-20 19:05:00 +0900

    home 220720 commit

--- src/main/java/com/example/demo/src/home/HomeController.java
+++ src/main/java/com/example/demo/src/home/HomeController.java
@@ -4,12 +4,15 @@ import com.example.demo.config.BaseException;
 import com.example.demo.config.BaseResponse;
 import com.example.demo.src.home.model.GetHomeRes;
 import com.example.demo.src.home.model.GetMenuRes;
+import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.example.demo.config.BaseResponseStatus.INVALID_USER_JWT;
 
 @RestController
 @RequestMapping("/home")
@@ -20,10 +23,13 @@ public class HomeController {
     private final HomeProvider homeProvider;
     @Autowired
     private final HomeService homeService;
+    @Autowired
+    private final JwtService jwtService;
 
-    public HomeController(HomeProvider homeProvider, HomeService homeService){
+    public HomeController(HomeProvider homeProvider, HomeService homeService, JwtService jwtService){
         this.homeProvider = homeProvider;
         this.homeService = homeService;
+        this.jwtService = jwtService;
     }
 
     /**
@@ -36,15 +42,26 @@ public class HomeController {
     @GetMapping("/") // (GET) 127.0.0.1:9000/home
     public BaseResponse<GetHomeRes> getHome(@RequestParam(required = false) int userIdx) {
         try{
+            //jwt에서 idx 추출.
+            int userIdxByJwt = jwtService.getUserIdx();
+            //userIdx와 접근한 유저가 같은지 확인
+            if(userIdx != userIdxByJwt){
+                return new BaseResponse<>(INVALID_USER_JWT);
+            }  // 이 부분까지는 유저가 사용하는 기능 중 유저에 대한 보안이 철저히 필요한 api 에서 사용
             // Get Users
             GetHomeRes getHomeRes = homeProvider.getHome();
             return new BaseResponse<>(getHomeRes);
 
         } catch(BaseException exception){
-            return new BaseResponse<>((exception.getStatus()));
+            return new BaseResponse<>(exception.getStatus());
         }
     }
 
+    /**
+     * 메뉴 상세 조회 API
+     * [GET] /menu/{menuIdx}
+     * @return BaseResponse<GetHomeRes>
+     */
     @ResponseBody
     @GetMapping("/menu/{menuIdx}") // (GET) 127.0.0.1:9000/home
     public BaseResponse<GetMenuRes> getMenu(@RequestParam int menuIdx) {
