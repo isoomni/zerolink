b2c29acdbc4b6a6c5f8150a454a4b2660b92136f
EUNJEONGMUN <munej26@gmail.com>
2022-07-20 13:18:50 +0900

    [ADD] edit restaurant object

--- src/main/java/com/example/demo/src/home/model/Restaurant.java
+++ src/main/java/com/example/demo/src/home/model/Restaurant.java
@@ -6,4 +6,7 @@ public class Restaurant {
     private int restaurantIdx;
     private String restaurantName;
     private double distance;
+    private String closeTime;
+    private String restaurantPhone;
+    private String status;
 }
